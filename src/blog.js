a4a6488d0798a0d1b3eb04e9b92fc31a37bb6092
alexpac27 <alexpac27@gmail.com>
2021-02-01 17:02:56 -0500

    flat method with arguments

--- src/blog.js
+++ src/blog.js
@@ -1,15 +1,27 @@
-// One level deep
-let originalArray = [1,2,[3,4]]           
-
-let flatArray = originalArray.flat()
-
-console.log(flatArray)                      // [1,2,3,4]
 
 // Two levels deep
-let twoLevels = [1,[2],[[3]]]            
+let twoLevels = [5,[6],[[7]]]            
 
 let flatTwoLevels = twoLevels.flat()
 
-console.log(flatTwoLevels)                  // [1,2,[3]]
+console.log(flatTwoLevels)                  // [5,6,[7]]
+
+
+// Five levels deep
+
+let fiveLevels = [2,[[2]],[[[[[2]]]]]]      // <-- The deepest array is nested 5 times
+
+let flatFiveLevels = fiveLevels.flat(5)     // <-- We need to go an minimum 5 levels deep to flatten this array
+
+console.log(flatFiveLevels)                 // [2,2,2]
+
+
+// If we go any less than 5 level, we end up with remaining nested arrays unflattened
+
+let almostFlatFive = fiveLevels.flat(4)     
+
+console.log(almostFlatFive)                 // [2,2,[2]]
+
+
 
 
