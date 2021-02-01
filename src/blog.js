e74b4d2f4c1dd380be2530120df42c588b8bbd92
alexpac27 <alexpac27@gmail.com>
2021-02-01 17:07:12 -0500

    four and five level arrays

--- src/blog.js
+++ src/blog.js
@@ -11,16 +11,18 @@ console.log(flatTwoLevels)                  // [5,6,[7]]
 
 let fiveLevels = [2,[[2]],[[[[[2]]]]]]      // <-- The deepest array is nested 5 times
 
-let flatFiveLevels = fiveLevels.flat(5)     // <-- We need to go an minimum 5 levels deep to flatten this array
-
-console.log(flatFiveLevels)                 // [2,2,2]
-
 
 // If we go any less than 5 level, we end up with remaining nested arrays unflattened
 
 let almostFlatFive = fiveLevels.flat(4)     
 
 console.log(almostFlatFive)                 // [2,2,[2]]
+
+// Fully flattened array
+
+let flatFiveLevels = fiveLevels.flat(5)     // <-- We need to go an minimum 5 levels deep to flatten this array
+
+console.log(flatFiveLevels)                 // [2,2,2]
 
 
 
