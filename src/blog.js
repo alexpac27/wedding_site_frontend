1d3bcdd890067e693c9e622953e09f3cf83e2fe3
alexpac27 <alexpac27@gmail.com>
2021-02-01 17:11:32 -0500

    infinity argument

--- src/blog.js
+++ src/blog.js
@@ -1,28 +1,12 @@
 
 // Two levels deep
-let twoLevels = [5,[6],[[7]]]            
+let manyLevels = [5,[[[[[[[[[6]]]]],[[[7]]]]]]],[[[[[[[[[[[[[[[[[8]]]]]]]]]]]]]]]]]]            
 
-let flatTwoLevels = twoLevels.flat()
+let flatManyLevels = manyLevels.flat(Infinity)
 
-console.log(flatTwoLevels)                  // [5,6,[7]]
-
-
-// Five levels deep
-
-let fiveLevels = [2,[[2]],[[[[[2]]]]]]      // <-- The deepest array is nested 5 times
+console.log(flatTwoLevels)                  // [5,6,7,8]
 
 
-// If we go any less than 5 level, we end up with remaining nested arrays unflattened
-
-let almostFlatFive = fiveLevels.flat(4)     
-
-console.log(almostFlatFive)                 // [2,2,[2]]
-
-// Fully flattened array
-
-let flatFiveLevels = fiveLevels.flat(5)     // <-- We need to go an minimum 5 levels deep to flatten this array
-
-console.log(flatFiveLevels)                 // [2,2,2]
 
 
 
