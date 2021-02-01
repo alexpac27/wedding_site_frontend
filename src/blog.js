c7507ec93b425277f5081269debe0840679abf9d
alexpac27 <alexpac27@gmail.com>
2021-02-01 16:50:28 -0500

    flat method examples

--- src/blog.js
+++ src/blog.js
@@ -1,6 +1,15 @@
 
-let originalArray = [1,2,[3,4]]
+let originalArray = [1,2,[3,4]]             // <-- This array is one level at its deepest
 
 let flatArray = originalArray.flat()
 
-console.log(flatArray)          // [1,2,3,4]
+console.log(flatArray)                      // [1,2,3,4]
+
+
+let twoLevels = [1,[2],[[3]]]               // <-- This array is two levels at its deepest
+
+let flatTwoLevels = twoLevels.flat()
+
+console.log(flatTwoLevels)                  // [1,2,[3]]
+
+
