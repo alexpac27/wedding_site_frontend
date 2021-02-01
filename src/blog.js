023db40694fc582bd02ddf6b925e36546e4d503d
alexpac27 <alexpac27@gmail.com>
2021-02-01 16:51:47 -0500

    examples modified

--- src/blog.js
+++ src/blog.js
@@ -1,12 +1,12 @@
-
-let originalArray = [1,2,[3,4]]             // <-- This array is one level at its deepest
+// One level deep
+let originalArray = [1,2,[3,4]]           
 
 let flatArray = originalArray.flat()
 
 console.log(flatArray)                      // [1,2,3,4]
 
-
-let twoLevels = [1,[2],[[3]]]               // <-- This array is two levels at its deepest
+// Two levels deep
+let twoLevels = [1,[2],[[3]]]            
 
 let flatTwoLevels = twoLevels.flat()
 
